bb067d8d05ad725aba0f0de0b30df785db789a6b
reyes <chris.reyes5@gmail.com>
2022-04-03 21:07:49 -0500

    update readme

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,3 @@
-# D3 Challenge - Data Journalism and D3
-Welcome to the newsroom! You've just accepted a data visualization position for a major metro paper. You're tasked with analyzing the current trends shaping people's lives, as well as creating charts, graphs, and interactive elements to help readers understand your findings.
-
-The editor wants to run a series of feature stories about the health risks facing particular demographics. She's counting on you to sniff out the first story idea by sifting through information from the U.S. Census Bureau and the Behavioral Risk Factor Surveillance System.
-
-The data set included with the assignment is based on 2014 ACS 1-year estimates from the [US Census Bureau](https://data.census.gov/cedsci/), but you are free to investigate a different data set. The current data set includes data on rates of income, obesity, poverty, etc. by state. MOE stands for "margin of error."
-
------
-
-## D3 Dabbler
-You need to create a scatter plot between two data variables.
-
-Using the D3 techniques we taught you in class, create a scatter plot that represents each state with circle elements. You'll code this graphic in the app.js file of your homework directory—make sure you pull in the data from data.csv by using the d3.csv function. 
-
-* Include state abbreviations in the circles.
-* Create and situate your axes and labels to the left and bottom of the chart.
-* Note: You'll need to use python -m http.server to run the visualization. This will host the page at localhost:8000 in your web browser.
-
-```js
 // D3 Animated Scatter Plot
 
 // Section 1: Pre-Data Setup
@@ -394,19 +375,8 @@ function visualize(theData) {
       // Remove highlight
       d3.select("." + d.abbr).style("stroke", "#e3e3e3");
     });
-```
 
------
-
-## Bonus: Impress the Boss
-* More Data, More Dynamics
-You're going to include more demographics and more risk factors. Place additional labels in your scatter plot and give them click events so that your users can decide which data to display. Animate the transitions for your circles' locations as well as the range of your axes.
-
-* Incorporate d3-tip
-While the ticks on the axes allow us to infer approximate values for each circle, it's impossible to determine the true value without adding another layer of data. Enter tooltips: developers can implement these in their D3 graphics to reveal a specific element's data when the user hovers their cursor over the element. Add tooltips to your circles and display each tooltip with the data that the user has selected. Use the d3-tip.js plugin developed by Justin Palmer.
-
-```js
-// Part 4: Make the Graph Dynamic
+  // Part 4: Make the Graph Dynamic
   // ==========================
   // This section will allow the user to click on any label
   // and display the data it references.
@@ -577,6 +547,3 @@ While the ticks on the axes allow us to infer approximate values for each circle
       .attr("r", circRadius / 3);
   }
 }
-```
-
------
